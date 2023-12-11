617a4652565621f51d9bfb9a4319ffadc1ca85d0
codegenius2 <lucifer231220@gmail.com>
2023-12-11 19:50:43 +0100

    style: added loading and other stuff

--- client/component/Modal/CheckoutMembership/CheckoutMembership.js
+++ client/component/Modal/CheckoutMembership/CheckoutMembership.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import styles from "../../../styles/modal-styles/modal-styles-content/checkout-membership.module.css";
+import Button from "../../Button/Button";
+import LoadingAnimation from "../../LoadingAnimation/LoadingAnimation";
 function CheckoutMembership() {
   const [isPlusToggled, setIsPlusToggled] = useState(false);
   const [isMinusToggled, setIsMinusToggled] = useState(false);
   const [quantityCount, setQuantityCount] = useState(1);
+  const [isLoading, setIsLoading] = useState();
 
   function handleVideoEnd(e) {
     e.target.play();
@@ -26,6 +29,14 @@ function CheckoutMembership() {
       setIsMinusToggled(!isMinusToggled);
     }
   }
+  function handleClickHaveWallet() {
+    setIsLoading(true);
+    console.log(isLoading);
+    setTimeout(() => {
+      setIsLoading(false);
+      console.log(isLoading);
+    }, 500);
+  }
   return (
     <div className={styles.checkout_membership_container}>
       <div className={styles.checkout_membership_video_and_selector_container}>
@@ -36,7 +47,7 @@ function CheckoutMembership() {
             muted
             onEnded={handleVideoEnd}
             // crossOrigin="anonymous"
-            src="https://firebasestorage.googleapis.com/v0 /b/philippe-gonet.appspot.com/o/V06_CPG_nft_1080x1080.mp4?alt=media&token=c988d654-d8ba-4bdd-ba51-c108986b0fa6"
+            src="https://firebasestorage.googleapis.com/v0/b/philippe-gonet.appspot.com/o/V06_CPG_nft_1080x1080.mp4?alt=media&token=c988d654-d8ba-4bdd-ba51-c108986b0fa6"
             // src="/videos/V06_CPG_nft_1080x1080.mp4"
           ></video>
         </div>
@@ -94,6 +105,22 @@ function CheckoutMembership() {
           Le Club Membre Philippe Gonet propose une expérience exclusive offerte
           par la maison de champagne
         </div>
+      </div>
+      <div className={styles.checkout_membership_buttons_container}>
+        {!isLoading ? (
+          <>
+            <div className={styles.checkout_membership_loading_container}>
+              <LoadingAnimation />
+            </div>
+          </>
+        ) : (
+          <>
+            <Button handleClick={handleClickHaveWallet} size="small">
+              J'ai un wallet
+            </Button>
+            <Button size="small">Je n'ai pas de wallet</Button>
+          </>
+        )}
       </div>
     </div>
   );
